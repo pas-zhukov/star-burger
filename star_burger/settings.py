1887ffbb935997b1f31b646582ac02578ed5cfc1
Pavel.WSL <pas-zhukov@yandex.ru>
2023-09-04 22:49:53 +0300

    setup postgres db

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -93,9 +93,11 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
-}
+            default='postgres://...',
+            conn_max_age=600,
+            conn_health_checks=True,)
+
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
