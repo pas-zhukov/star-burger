48551b01f837bec2d48dcffe627b93b48855b8ac
Pavel.Notebook <pas-zhukov@yandex.ru>
2023-09-07 23:39:03 +0300

    finish deploy script

--- upgrade_and_deploy.sh
+++ upgrade_and_deploy.sh
@@ -1,3 +1,55 @@
 #!/usr/bin/env bash
+set -e
+source ./.env #import environment variables
+
+working_directory=$(pwd)
+echo "Cd to /opt/projects/star-burger..."
+cd /opt/projects/star-burger/
 
 echo "Check for updates on remote git repository."
+git fetch
+ahead_commits=$(git rev-list HEAD...origin/master --count)
+if [[ $ahead_commits > 0 ]]
+then
+  echo "Pulling new updates."
+  git pull
+else
+  echo "Already up to date."
+  exit 0
+fi
+
+echo "Install Python dependencies."
+./venv/bin/pip install -r requirements.txt
+
+echo "Install NodeJS libraries."
+npm ci --dev
+
+echo "Build js code."
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+echo "Collect Django static."
+./venv/bin/python3 manage.py collectstatic --noinput
+
+echo "Apply Django migrations."
+./venv/bin/python3 manage.py migrate
+
+echo "Restart systemd services."
+systemctl restart star-burger.service
+systemctl restart postgresql.service
+
+echo "Send deployment info to Rollbar."
+sha=$(git rev-parse HEAD)
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header "X-Rollbar-Access-Token: '${ROLLBAR_TOKEN}'" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+              {
+                "environment": "production",
+                "revision": "'${sha}'"
+              }
+            '
+
+echo "Deployment completed!"
+cd $working_directory
